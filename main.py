465b01fb8942dbcc2d9eb7466375b470d99de931
Nombulelo <nombulelokhumalo48@gmail.com>
2024-09-28 02:23:57 +0200

    Added results display
    Added extra function (create_grid).

--- main.py
+++ main.py
@@ -6,27 +6,35 @@ class TicTacToeApp:
         self.root = root
         self.root.title("Tic-Tac_Toe")
         self.root.iconbitmap('crown_icon.ico')
-        self.root.geometry("400x400")
+        self.root.geometry("400x500")
 
-        self.board = [None] * 9 
+        self.board_size = 3 
+        self.board = [None] * (self.board_size * self.board_size)
         self.current_player = "X"
+
+        self.score_X = 0
+        self.score_O = 0
 
         self.title_label = tk.Label(root, text = "Tic-Tac-Toe", font=("Arial",20))
         self.title_label.pack(pady = 10)
 
+        self.score_label = tk.Label(root, text= f"Player X: {self.score_X} | Player O: {self.score_O}", font=("Arial", 14))
+        self.score_label.pack(pady=10)
+
         self.grid_frame = tk.Frame(root)
         self.grid_frame.pack()
 
-        #buttons
-        self.buttons = []
-        for i in range(9):
-            button = tk.Button(self.grid_frame, text="", width=10, height=4, font=("Arial", 16), command=lambda i=i: self.on_button_click(i))
-            button.grid(row=i//3, column=i%3)
-            self.buttons.append(button)
+        self.create_grid()
 
-        #reset button
         self.reset_button = tk.Button(root, text="Reset Game", font=("Arial",14), command=self.reset_game)
         self.reset_button.pack(pady=20)
+
+    def create_grid(self):
+        self.buttons = []
+        for i in range(self.board_size * self.board_size):
+            button = tk.Button(self.grid_frame, text="", width=10, height=4, font=("Arial", 16), command=lambda i=i: self.on_button_click(i))
+            button.grid(row=i//self.board_size, column=i%self.board_size)
+            self.buttons.append(button)
 
     def on_button_click(self, index):
         if self.buttons[index]["text"] == "" and not self.check_winner():
@@ -36,6 +44,7 @@ class TicTacToeApp:
             #checking winner
             if self.check_winner():
                 messagebox.showinfo("Game Over", f"player {self.current_player} wins!!")
+                self.update_score(self.current_player)
                 self.disable_buttons()
             elif None not in self.board:
                 messagebox.showinfo("Game Over", "It's a draw!")
@@ -43,6 +52,13 @@ class TicTacToeApp:
             else:
                 self.current_player = "O" if self.current_player == "X" else "X"
 
+    def update_score(self, player):
+        if player == "X":
+            self.score_X += 1
+        else:
+            self.score_O += 1
+        self.score_label.config(text= f"Player X: {self.score_X} | Player O: {self.score_O}")
+    
     def check_winner(self):
         winning_combination = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], 
@@ -52,13 +68,14 @@ class TicTacToeApp:
         for combo in winning_combination:
             if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] and self.board[combo[0]] is not None:
                 return True
+        return False
 
     def disable_buttons(self):
         for button in self.buttons:
             button.config(state = "disabled")
 
     def reset_game(self):
-        self.board = [None] * 9
+        self.board = [None] * (self.board_size * self.board_size)
         self.current_player = "X"
 
         for button in self.buttons:
